ad9c944e771448d55470ae962a90a49e05e89180
NonSinat <sinat.non@student.niptict.edu.kh>
2021-09-30 16:47:57 +0700

    close 1 Notification DarkMode

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:note_application_flutter/services/notification_services.dart';
 import 'package:note_application_flutter/services/theme_services.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +12,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.requestIOSPermissions();
+    notifyHelper.initializeNotification();
+  }
+
+  @override
+  void dispose() {
+   
+    super.dispose();
+       SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top,SystemUiOverlay.bottom]);
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +46,13 @@ class _HomePageState extends State<HomePage> {
         onPressed: () {
           debugPrint("tabed");
           ThemeServices().swithcTheme();
+          notifyHelper.displayNotification(
+            title: "Theme changed",
+            body: Get.isDarkMode
+                ? "Activated Light Theme"
+                : "Activated Dark Theme",
+          );
+          notifyHelper.scheduledNotification();
         },
         icon: Icon(
           Icons.nightlight_round,

b3f7751252139f36f8d8cf3a5e755ab512085e07
NonSinat <sinat.non@student.niptict.edu.kh>
2021-10-02 21:17:05 +0700

    add await

--- lib/db/db_hepler.dart
+++ lib/db/db_hepler.dart
@@ -15,9 +15,9 @@ class DBHelper {
       _db = await openDatabase(
         _path,
         version: _version,
-        onCreate: (db, version) {
+        onCreate: (db, version) async {
           print("Creating a new database one");
-          return db.execute(
+          return await db.execute(
             "CREATE TABLE $_tableName(id INTEGER PRIMARY KEY AUTOINCREMENT,title STRING,note TEXT,date STRING,startTime STRING,endTime STRING,remind INTEGER,repeat STRING,color INTEGER,isCompleted INTEGER)",
           );
         },
@@ -29,7 +29,30 @@ class DBHelper {
 
   static Future<int?> insert(Task? task) async {
     print("insert function called");
+    await initDb();
 
-    return await _db?.insert(_tableName, task!.toJson())??1;
+    return await _db?.insert(_tableName, task!.toJson()) ?? 1;
+  }
+
+  static Future<List<Task>> getTaskList() async {
+    await initDb();
+    final List<Map<String, dynamic>> maps = await _db!.query('task');
+    return List.generate(
+      maps.length,
+      (index) {
+        return Task(
+          id: maps[index]['id'],
+          title: maps[index]['title'],
+          date: maps[index]['date'],
+          note: maps[index]['note'],
+          isCompleted: maps[index]['isCompleted'],
+          startTime: maps[index]['startTime'],
+          endTime: maps[index]['endTime'],
+          color: maps[index]['color'],
+          remind: maps[index]['remind'],
+          repeat: maps[index]['repeat'],
+        );
+      },
+    );
   }
 }
